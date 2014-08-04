ea2060ca7599765125f1d9aea5b06548c28ed093
Henrik <henrik.lafrenz@ableton.com>
2014-08-04 21:08:27 +0200

    Initial commit

---
+++ js/lib.js
@@ -0,0 +1,18 @@
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var num_tracks = 3;
+
+function getRandomExcept(blacklist) {
+    while (true) {
+	var num = getRandomInt(0, num_tracks - 1);
+	if (blacklist.indexOf(num) < 0) {
+	    return num;
+	}
+    }
+}
